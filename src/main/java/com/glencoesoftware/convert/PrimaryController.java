fac101cd08a8f37d3cbfd9600a5280a8171b4e95
DavidStirling <daves1990@hotmail.co.uk>
2022-02-02 14:21:06 +0000

    Fix line duplication

--- src/main/java/com/glencoesoftware/convert/PrimaryController.java
+++ src/main/java/com/glencoesoftware/convert/PrimaryController.java
@@ -273,7 +273,6 @@ public class PrimaryController {
         fxmlLoader.getNamespace().put("guiVer", version);
         fxmlLoader.getNamespace().put("b2rVer", Converter.class.getPackage().getImplementationVersion());
         fxmlLoader.getNamespace().put("bfVer", ImageReader.class.getPackage().getImplementationVersion());
-        fxmlLoader.getNamespace().put("bfVer", ImageReader.class.getPackage().getImplementationVersion());
         fxmlLoader.setLocation(App.class.getResource("AboutDialog.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         Stage stage = new Stage();
