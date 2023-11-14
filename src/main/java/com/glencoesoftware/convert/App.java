a2933debc501da3b854cb4e8dca1c0136f658e91
David Stirling <dstirling@glencoesoftware.com>
2023-11-14 11:05:13 +0000

    Copyright statements

--- src/main/java/com/glencoesoftware/convert/App.java
+++ src/main/java/com/glencoesoftware/convert/App.java
@@ -1,6 +1,5 @@
 /**
- * Copyright (c) 2022 Glencoe Software, Inc. All rights reserved.
- *
+ * Copyright (c) 2023 Glencoe Software, Inc. All rights reserved.
  * This software is distributed under the terms described by the LICENSE.txt
  * file you can find at the root of the distribution bundle.  If the file is
  * missing please request a copy by contacting info@glencoesoftware.com
--- src/main/java/com/glencoesoftware/convert/dialogs/ConfigureJobDialog.java
+++ src/main/java/com/glencoesoftware/convert/dialogs/ConfigureJobDialog.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2023 Glencoe Software, Inc. All rights reserved.
+ * This software is distributed under the terms described by the LICENSE.txt
+ * file you can find at the root of the distribution bundle.  If the file is
+ * missing please request a copy by contacting info@glencoesoftware.com
+ */
 package com.glencoesoftware.convert.dialogs;
 
 import com.fasterxml.jackson.core.*;
